b0eee79b264bd73801ca8c36b5a3d3cffc33c92e
saafaaari <111dltldnjs@naver.com>
2022-06-28 23:52:31 +0900

    예매 정보를 담고있는 Reservation 타입을 반환하는 메서드 구현 p.46

--- Objact_Ch2/Objact_Ch2/Sreening.swift
+++ Objact_Ch2/Objact_Ch2/Sreening.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-print("Hello, World!")
-
 class Sreening {
     private let movie: Movie
     private let sequence: Int
@@ -31,4 +29,10 @@ class Sreening {
     func getMovieFee() -> Money {
         return movie.getFee()
     }
+    
+    //예매 정보를 담고있는 Reservation 타입을 반환하는 메서드
+    func reserve(customer: Customer/*예매자의 정보를 담고있는 타입*/, audienceCount: Int/*인원수*/) -> Reservation {
+        return Reservation()
+    }
 }
+// 프로퍼티를 은닉화하고 메서드를 오픈함으로써 객체의 자율성을 보장한다.
